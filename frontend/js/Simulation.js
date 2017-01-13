6bc7f8a9a689d0e5500a0709a15d41ce6388e4e9
pgaret <peregringaret@gmail.com>
2017-01-13 03:34:15 -0500

    Pause button pauses, going in reverse restores taken pieces

--- frontend/js/Simulation.js
+++ frontend/js/Simulation.js
@@ -5,26 +5,39 @@ class Simulation{
     this.past_moves_board = simulation["past_moves_board"]
     this.board_size = [this.game_board["rows"], this.game_board["cols"]]
     this.current = this.past_moves_board.length-2
-    this.delay = 1000
+    this.delay = 500
+    this.direction = 1
+    this.runningSim = false
+    this.takenPieces=[]
+    this.paused = false
     this.setBoard(this.game_board["rows"], this.game_board["cols"], this.past_moves_board[this.past_moves_board.length-1]["pieces"], this.past_moves_board[0]["special_square"])
   }
 
   runSim(){
+    this.runningSim = true
     setTimeout(function render(){
-      debugger
-      let fromSpace; let toSpace
-      if (simulator.current >= 0) {
-        fromSpace = [simulator.past_moves_board[simulator.current]["last_move_squares"]["from"]["row"], simulator.past_moves_board[simulator.current]["last_move_squares"]["from"]["col"]]
-        toSpace = [simulator.past_moves_board[simulator.current]["last_move_squares"]["to"]["row"], simulator.past_moves_board[simulator.current]["last_move_squares"]["to"]["col"]]
-      } else {
-        fromSpace = [simulator.game_board["board"]["last_move_squares"]["from"]["row"], simulator.game_board["board"]["last_move_squares"]["from"]["col"]]
-        toSpace = [simulator.game_board["board"]["last_move_squares"]["to"]["row"], simulator.game_board["board"]["last_move_squares"]["to"]["col"]]
-      }
-      let index = simulator.getId(fromSpace)
-      simulator.changeBoard(fromSpace, toSpace)
-      if (!simulator.paused && simulator.current >= 0){
-        simulator.current -= 1
-        setTimeout(render, simulator.delay)
+      // debugger
+      if (!simulator.paused){
+        if ((simulator.direction > 0 && simulator.current > -2 && simulator.current+1 < simulator.past_moves_board.length) || (simulator.direction < 0 && simulator.current > -2 && simulator.current+1 < simulator.past_moves_board.length)){
+          let fromSpace; let toSpace
+          if (simulator.current >= 0) {
+            fromSpace = [simulator.past_moves_board[simulator.current]["last_move_squares"]["from"]["row"], simulator.past_moves_board[simulator.current]["last_move_squares"]["from"]["col"]]
+            toSpace = [simulator.past_moves_board[simulator.current]["last_move_squares"]["to"]["row"], simulator.past_moves_board[simulator.current]["last_move_squares"]["to"]["col"]]
+          } else {
+            fromSpace = [simulator.game_board["board"]["last_move_squares"]["from"]["row"], simulator.game_board["board"]["last_move_squares"]["from"]["col"]]
+            toSpace = [simulator.game_board["board"]["last_move_squares"]["to"]["row"], simulator.game_board["board"]["last_move_squares"]["to"]["col"]]
+          }
+          simulator.changeBoard(fromSpace, toSpace)
+          this.runningSim = false
+          setTimeout(render, simulator.delay)
+          simulator.current -= simulator.direction
+        }
+        else {
+          if (simulator.current === -2) {simulator.current += 1}
+          simulator.runningSim = false
+          $("#play").css("display", "inline-block")
+          $("#pause").css("display", "none")
+        }
       }
     }, simulator.delay)
   }
@@ -39,12 +52,11 @@ class Simulation{
     for (let i = 0; i < rows; i++){
       for (let j = 0; j < cols; j++){
         let space_class = "spot"
-        // debugger
         let img_src = "<img id='piece' src='./css/"
         i%2 === j%2 ? space_class += " white" : space_class += " black"
         if (special_square) { if (special_square['row'] === i+1 && special_square['col'] === j+1) { space_class += " special" } }
         if (pieces[i*cols+j] !== 'e'){
-          img_src += pieces[i*cols+j].toUpperCase() === pieces[i*cols+j] ? 'W' : 'B'
+          img_src += pieces[i*cols+j].toUpperCase() === pieces[i*cols+j] ? 'B' : 'W'
           img_src += (pieces[i*cols+j].toUpperCase()+".png'></img>")
         } else {
           img_src = "<span id='piece'></span>"
@@ -56,9 +68,41 @@ class Simulation{
   }
 
   changeBoard(fromSpace, toSpace){
-    let fromIndex = this.getId(fromSpace); let toIndex = this.getId(toSpace)
-    $("#"+toIndex).empty().prepend($("#"+fromIndex).children()[0])
-    $("#"+fromIndex).prepend("<span id='piece'></span>")
+    // debugger
+    let fromIndex; let toIndex
+    if (this.direction === 1){
+      fromIndex = this.getId(fromSpace); toIndex = this.getId(toSpace)
+    } else {
+      fromIndex = this.getId(toSpace); toIndex = this.getId(fromSpace)
+    }
+    let orig_copy = $("#"+fromIndex).children()
+    let temp = orig_copy.clone().appendTo('body')
+    if (!$("#"+toIndex).children().is('span')){
+      let img_src = $("#"+toIndex).children().prop('src')
+      img_src = "."+img_src.slice(img_src.length - 11, img_src.length)
+      this.takenPieces.push([img_src, this.current, this.direction])
+    }
+    $("#"+toIndex).empty()
+    let new_copy = orig_copy.clone().appendTo("#"+toIndex)
+    let oldOffset = orig_copy.offset()
+    $("#"+fromIndex).empty().append("<span id='piece'></span>")
+    let newOffset = new_copy.offset()
+    for (let i = 0; i < this.takenPieces.length; i++){
+      if (this.takenPieces[i][1] === this.current && this.takenPieces[i][2] !== this.direction){
+        // debugger
+        $("#"+fromIndex).empty().append(`<img sid="piece" src=${this.takenPieces[i][0]}></img>`)
+      }
+    }
+    temp
+      .css('position', 'absolute')
+      .css('left', oldOffset.left)
+      .css('top', oldOffset.top)
+      .css('zIndex', 1000)
+    new_copy.css("display", "none")
+    temp.animate({'top': newOffset.top, 'left':newOffset.left}, this.delay, function(){
+       new_copy.css("display", "block")
+       temp.remove()
+    })
   }
 
 }
--- frontend/js/app.js
+++ frontend/js/app.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
     url: 'http://chess-puzzles.herokuapp.com/api/v1/puzzles',
     success: function(results){
       $("#puzzle").html(results)
-      console.log(results)
       for (let i = 0; i < results.length; i++){
         let puzzle_data = results[i]["game_board"]["Puzzle_No"]+" | "+results[i]["game_board"]["Rating"]+" | "+results[i]["game_board"]["Played_times"]
         let str = `<button onClick='simulate(${results[i]['ind']})'>${puzzle_data}</button><br>`
@@ -30,5 +29,21 @@ function simulate(id){
 }
 
 function runSim(){
-  simulator.runSim()
+  $("#pause").css("display", "inline-block")
+  $("#play").css("display", "none")
+  simulator.paused = false
+  setTimeout(simulator.runSim(), 1)
+}
+
+function pauseSim(){
+  simulator.paused = true
+  $("#play").css("display", "inline-block")
+  $("#pause").css("display", "none")
+}
+
+function changeSimOptions(speed, direction){
+  simulator.delay = speed
+  simulator.direction = direction
+  simulator.paused = false
+  if (!simulator.runningSim) {runSim()}
 }
